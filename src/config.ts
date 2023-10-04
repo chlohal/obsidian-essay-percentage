b3a9b18c86bd516d05e05f511286541af42c6d22
chlohal <me@clh.sh>
2023-10-04 09:45:28 -0400

    new essay just dropped

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
-const STATUS_FORMATTED = "i am paulina & essay {}% done";
+const STATUS_FORMATTED = "i am paulina & essay due tomorrow. {}% done if i don't reply this is why im sorry";
 
 export const UPDATE_INTERVAL_S = 60;
 
-export const ESSAY_TARGET = 2000;
+export const ESSAY_TARGET = 1500;
 
 export function formatMessage(percent: number) {
     return STATUS_FORMATTED.replace("{}", `${round5(percent)}`);
